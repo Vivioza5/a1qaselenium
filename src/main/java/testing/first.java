1c10072d1d9116103294b97d19519f3b7d6d317a
Vitaliy <v.vivian@qa-academy.by>
2021-11-24 15:27:21 +0200

    first start

--- src/main/java/testing/first.java
+++ src/main/java/testing/first.java
@@ -3,13 +3,19 @@ package testing;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class first {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver webDriver = new ChromeDriver();
-        String url ="https://freeteamclub.com/forum/";
+        String url ="https://google.com";
         webDriver.get(url);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         webDriver.close();
     }
 
